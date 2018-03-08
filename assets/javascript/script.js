0d93937060494059a541ead92124808849f2ff43
codemode9 <timourbasegmez@hotmail.com>
2018-03-07 20:47:38 -0500

    semi-pseudo green

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 
-//*******************Nav Bar*****************************//
+//*******************Nav Bar Render*************************//
 
     $(document.body).append(`<nav class="navbar navbar-light " style="background-color:#EB695E">
     <a class="navbar-brand" style="color: white" href="#">
       EOA Zombies
     </a>
   </nav>`);
-    
+
+//***************End Nav Bar Render************************//
+
+// left nav bar for locations btn, weapons btn, portals btn(maybe) etc...
+
+// clear main div and dependant on btn clicked rend more buttons or show guide
+
 });
